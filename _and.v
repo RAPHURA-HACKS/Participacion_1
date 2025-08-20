60ed1c21459eb920c64461b9f0bf7030d7aea1ef
RAPHURA-HACKS <megustaeldinero1200@gmail.com>
2025-08-20 16:08:07 -0600

    Se agrega el proyecto

---
+++ _and.v
@@ -0,0 +1,9 @@
+//1.Definicion del modulo, sus entradas y salidas
+//Dentro del parentesis se define los 1/0
+module _and (input a, input b, output c);
+//2.Definen cables o componentes internos
+//N/A
+//3.Asignaciones, instancias y conexiones
+
+assign c= a & b;
+endmodule
